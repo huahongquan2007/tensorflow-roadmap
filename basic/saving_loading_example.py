0da474a00307c71f6f56327e1592a528d0f1fa3f
Quan Hua <quanhua92@gmail.com>
2017-01-11 20:27:54 +0700

    Add basic tensorflow 1.0

---
+++ basic/saving_loading_example.py
@@ -0,0 +1,33 @@
+import tensorflow as tf
+
+sess = tf.InteractiveSession()
+
+raw_data = [1., 2., 8., -1, 0, 5.5, 7, 13]
+
+spikes = tf.Variable([False] * len(raw_data), name='spikes')
+spikes.initializer.run()
+
+saver = tf.train.Saver()
+
+for i in range(1, len(raw_data)):
+    if raw_data[i] - raw_data[i - 1] > 5:
+        spikes_val = spikes.eval()
+        spikes_val[i] = True
+        updater = tf.assign(spikes, spikes_val)
+        updater.eval()
+
+save_path = saver.save(sess, "/tmp/spikes.ckpt")
+print("spikes data saved in file: %s" % save_path)
+
+sess.close()
+
+tf.reset_default_graph() # delete all variables in the current graph
+sess = tf.InteractiveSession()
+spikes = tf.Variable([False] * 8, name='spikes')
+# spikes.initializer.run() # don't need to run this
+saver = tf.train.Saver()
+
+saver.restore(sess, "/tmp/spikes.ckpt")
+print("spikes data", spikes.eval())
+
+sess.close()
---
+++ basic/tensors_example.py
@@ -0,0 +1,72 @@
+import tensorflow as tf
+import numpy as np
+
+m1 = [[1.0, 2.0],
+      [3.0, 4.0]]
+
+m2 = np.array([[1.0, 2.0],
+               [3.0, 4.0]], dtype=np.float32)
+
+m3 = tf.constant([[1.0, 2.0],
+                  [3.0, 4.0]])
+
+print(type(m1))
+print(type(m2))
+print(type(m3))
+
+
+print("----------")
+t1 = tf.convert_to_tensor(m1, dtype=tf.float32)
+t2 = tf.convert_to_tensor(m2, dtype=tf.float32)
+t3 = tf.convert_to_tensor(m3, dtype=tf.float32)
+
+print(type(t1))
+print(type(t2))
+print(type(t3))
+
+
+
+print("----------")
+matrix1 = tf.constant([[1.0, 2.0]])
+matrix2 = tf.constant([[1], [2]])
+
+myTensor = tf.constant([
+    [[1,2 ],
+     [3,4],
+     [5,6]],
+    [[7,8],
+     [9,10],
+     [11,12]]
+])
+
+matrix_one = tf.ones((3,4))
+matrix_zeros = tf.zeros((3,4))
+matrix_ten = tf.ones((3, 4)) * 10
+
+print(matrix1)
+print(matrix2)
+print(myTensor)
+print(matrix_one)
+print(matrix_zeros)
+print(matrix_ten)
+
+
+print("----------")
+x = tf.constant([[1, 2]])
+neg_x = tf.negative(x)
+print(neg_x)
+
+print("---- Session ---- ")
+with tf.Session() as sess:
+    result = sess.run(neg_x)
+print(result)
+
+sess = tf.InteractiveSession()
+
+matrix = tf.constant([[1., 2.]])
+negMatrix = tf.negative(matrix)
+
+result = negMatrix.eval()
+
+print(result)
+sess.close()
